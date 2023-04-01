937258811dbc9a730b96dff456b18aa06f7af925
Nik <niky006@gmail.com>
2023-04-01 16:25:24 +0200

    fix: use the correct class for date and added constructors

--- src/main/java/com/nik7/littlejohn/resurce/TickerHistory.java
+++ src/main/java/com/nik7/littlejohn/resurce/TickerHistory.java
@@ -4,20 +4,28 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.nik7.littlejohn.databind.DoubleToStringSerializer;
 
-import java.util.Locale;
+import java.time.LocalDate;
 import java.util.StringJoiner;
 
 public class TickerHistory {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private Locale date;
+    private LocalDate date;
     @JsonSerialize(using = DoubleToStringSerializer.class)
     private double price;
 
-    public Locale getDate() {
+    public TickerHistory() {
+    }
+
+    public TickerHistory(LocalDate date, double price) {
+        this.date = date;
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(Locale date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 
